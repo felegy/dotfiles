5ece94e9ed4748264abc3b2a0b8a70a4ffbbb807
felegy <felegy@hvg.hu>
2020-10-09 18:17:45 +0200

    + darwin OS support

--- profile.d/brew.sh
+++ profile.d/brew.sh
@@ -1,1 +1,4 @@
-eval $(/home/linuxbrew/.linuxbrew/bin/brew shellenv)
+case $(uname) in
+    Darwin) eval $( $HOME/homebrew/bin/brew shellenv);;
+    *)      eval $(/home/linuxbrew/.linuxbrew/bin/brew shellenv);;
+esac
--- profile.d/nvm.sh
+++ profile.d/nvm.sh
@@ -1,4 +1,9 @@
+case $(uname) in
+    Darwin) BREW_DIR=$HOME/homebrew;;
+    *)      BREW_DIR=/home/linuxbrew/.linuxbrew;;
+esac
+
 export NVM_DIR="$HOME/.nvm"
-[ -s "/home/linuxbrew/.linuxbrew/opt/nvm/nvm.sh" ] && . "/home/linuxbrew/.linuxbrew/opt/nvm/nvm.sh"  # This loads nvm
-[ -s "/home/linuxbrew/.linuxbrew/opt/nvm/etc/bash_completion.d/nvm" ] && . "/home/linuxbrew/.linuxbrew/opt/nvm/etc/bash_completion.d/nvm"  # This loads nvm bash_completion
+[ -s "$BREW_DIR/opt/nvm/nvm.sh" ] && . "$BREW_DIR/opt/nvm/nvm.sh"  # This loads nvm
+[ -s "$BREW_DIR/opt/nvm/etc/bash_completion.d/nvm" ] && . "$BREW_DIR/opt/nvm/etc/bash_completion.d/nvm"  # This loads nvm bash_completion
 
